062a5ca6e9b70137eb86950396626358fd871979
nonwill <nonwill@hotmail.com>
2025-05-27 21:35:33 +0800

    GCC 14.3

--- mingw-w64+gcc.sh
+++ mingw-w64+gcc.sh
@@ -6,7 +6,7 @@ export GMPSVer=6.3.0
 export MPFRSVer=4.2.2
 export MPCSVer=1.3.1
 export ISLSVer=0.27
-export GCCSVer=14.2.0
+export GCCSVer=14.3.0
 export MINGWSVer=v12.0.0
 
 if [ ! -f "gcc-${GCCSVer}.tar" ];then
